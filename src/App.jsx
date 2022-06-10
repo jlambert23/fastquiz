cddabf0ce105e61b8c87a697c3fbc246ba699786
Jay <jb.lambert23@gmail.com>
2022-06-10 18:32:53 -0400

    implement answers

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import {
   Container,
   Group,
   MantineProvider,
+  Stack,
   Title,
 } from '@mantine/core';
 import { decode } from 'html-entities';
 
 function App() {
   const [quiz, setQuiz] = useState();
+  const [result, setResult] = useState(true);
 
   useEffect(() => {
     getQuiz();
@@ -21,14 +23,25 @@ function App() {
       'https://opentdb.com/api.php?amount=1'
     ).then((res) => res.json());
     const q = results[0];
+    const i = Math.floor(Math.random() * 2);
+    const options = [];
+    options[i] = decode(q.correct_answer);
+    options[(i + 1) % 2] = decode(q.incorrect_answers[0]);
     setQuiz({
       question: decode(q.question),
-      options: [decode(q.correct_answer), decode(q.incorrect_answers[0])],
       answer: decode(q.correct_answer),
+      options,
     });
+    setResult(null);
   };
 
-  const handleClick = () => {
+  const wait = (ms) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
+  const handleClick = async (guess) => {
+    setResult(guess === quiz.answer);
+    await wait(1000);
     getQuiz();
   };
 
@@ -50,17 +63,27 @@ function App() {
           sx={(theme) => ({ backgroundColor: theme.colors.dark[6] })}
           shadow
         >
-          <Title align='center' mb='md'>
-            {quiz?.question}
-          </Title>
-          <Group position='center'>
-            {quiz &&
-              quiz.options.map((o) => (
-                <Button key={o} size='xl' onClick={handleClick}>
-                  {o}
-                </Button>
-              ))}
-          </Group>
+          <Stack spacing='lg'>
+            <Title align='center'>{quiz?.question}</Title>
+            {result !== null && (
+              <Title
+                align='center'
+                sx={(theme) => ({
+                  color: result ? theme.colors.green[7] : theme.colors.red[7],
+                })}
+              >
+                {result ? 'Correct!' : 'Wrong!'}
+              </Title>
+            )}
+            <Group position='center'>
+              {quiz &&
+                quiz.options.map((o) => (
+                  <Button key={o} size='xl' onClick={() => handleClick(o)}>
+                    {o}
+                  </Button>
+                ))}
+            </Group>
+          </Stack>
         </Card>
       </Container>
     </MantineProvider>
